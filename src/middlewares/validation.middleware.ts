9a9b515f3e6d58ca7bc0666bba6ad03c3e0e3d48
Jeffrey Alahira <alahirajeffrey@gmail.com>
2025-01-26 23:17:35 +0100

    middleware to validate request bodies

---
+++ src/middlewares/validation.middleware.ts
@@ -0,0 +1,8 @@
+const validate = (schema: any) => (req: any, res: any, next: any) => {
+  try {
+    schema.parse(req.body);
+    next();
+  } catch (error: any) {
+    return res.status(400).json({ error: error.errors });
+  }
+};
