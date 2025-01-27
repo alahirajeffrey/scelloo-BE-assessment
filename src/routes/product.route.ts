77c0523144f658812822606621914b11288526e9
Jeffrey Alahira <alahirajeffrey@gmail.com>
2025-01-27 12:04:25 +0100

    commit

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,5 +1,41 @@
 import { Router } from "express";
+import { validate } from "../middlewares";
+import { createProductValidation } from "../validations";
+import asyncHandler from "express-async-handler";
+import { isLoggedIn, isAdmin } from "../middlewares";
 
 const productRouter = Router();
+
+productRouter.post(
+  "",
+  isLoggedIn,
+  isAdmin,
+  validate(createProductValidation),
+  asyncHandler(async (req: Request, res: Response) => {})
+);
+
+productRouter.patch(
+  "",
+  validate(createProductValidation),
+  asyncHandler(async (req: Request, res: Response) => {})
+);
+
+productRouter.get(
+  "/all",
+  validate(createProductValidation),
+  asyncHandler(async (req: Request, res: Response) => {})
+);
+
+productRouter.get(
+  "/:productId",
+  validate(createProductValidation),
+  asyncHandler(async (req: Request, res: Response) => {})
+);
+
+productRouter.delete(
+  "/:productId",
+  validate(createProductValidation),
+  asyncHandler(async (req: Request, res: Response) => {})
+);
 
 export { productRouter };
