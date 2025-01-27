66654be810fdf0ec406891173e0f2dfae6beb88a
Jeffrey Alahira <alahirajeffrey@gmail.com>
2025-01-27 12:03:08 +0100

    commit

--- src/middlewares/login-limiter.midelleware.ts
+++
@@ -1,8 +0,0 @@
-import rateLimit from "express-rate-limit";
-
-// limit to 5 requests every 10 minutes
-export const loginLimiter = rateLimit({
-  windowMs: 10 * 60 * 1000,
-  max: 5,
-  message: "Too many login attempts. Try again later.",
-});
