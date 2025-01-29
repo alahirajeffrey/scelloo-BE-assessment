69a442482762f8936aabf377f5d728ebaf2dab15
Jeffrey Alahira <alahirajeffrey@gmail.com>
2025-01-29 14:34:37 +0100

    added endoint to create product

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,8 +1,9 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 import { validate } from "../middlewares";
 import { createProductValidation } from "../validations";
 import asyncHandler from "express-async-handler";
 import { isLoggedIn, isAdmin } from "../middlewares";
+import { Product } from "../models";
 
 const productRouter = Router();
 
@@ -11,7 +12,14 @@ productRouter.post(
   isLoggedIn,
   isAdmin,
   validate(createProductValidation),
-  asyncHandler(async (req: Request, res: Response) => {})
+  asyncHandler(async (req: Request, res: Response) => {
+    try {
+      const product = await Product.create(req.body);
+      res.status(201).json(product);
+    } catch (error: any) {
+      res.status(500).json({ message: error.message });
+    }
+  })
 );
 
 productRouter.patch(
