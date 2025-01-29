19a5b0b8f0507dfe3d5048b4fe671901c233a290
Jeffrey Alahira <alahirajeffrey@gmail.com>
2025-01-29 14:42:41 +0100

    added route to update product

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,6 +1,9 @@
 import { Router, Request, Response } from "express";
 import { validate } from "../middlewares";
-import { createProductValidation } from "../validations";
+import {
+  createProductValidation,
+  uppdateProductValidation,
+} from "../validations";
 import asyncHandler from "express-async-handler";
 import { isLoggedIn, isAdmin } from "../middlewares";
 import { Product } from "../models";
@@ -24,8 +27,21 @@ productRouter.post(
 
 productRouter.patch(
   "",
-  validate(createProductValidation),
-  asyncHandler(async (req: Request, res: Response) => {})
+  isLoggedIn,
+  isAdmin,
+  validate(uppdateProductValidation),
+  asyncHandler(async (req: Request, res: Response) => {
+    try {
+      const product = await Product.findByPk(req.params.id);
+      if (!product) {
+        return res.status(404).json({ error: "Product not found" });
+      }
+      await product.update(req.body);
+      res.json(product);
+    } catch (error: any) {
+      res.status(400).json({ error: error.message });
+    }
+  })
 );
 
 productRouter.get(
